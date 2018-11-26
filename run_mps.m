4d0daf85c9501757f2e8736d74e2620efa2e7ed9
Chaoyi <yangc22@uw.edu>
2018-11-25 17:32:09 -0800

    test results

--- run_mps.m
+++ run_mps.m
@@ -1,9 +1,17 @@
 clear variables, close all, clc
 
-test = {'ex2ocp'};
+filename_info = 'test_result';
+fileID_info = fopen(filename_info,'w');
+fprintf(fileID_info, '%s\n', 'Test Results with Collocation:');
+
+examples_Dir = '../examples/';
+test = {'ex7aocp'};
+
 for i = 1 : size(test, 2)
     filename = char(test(i));
-    [y0, z0, para, t0, tf, t_span, outputFile, alpham, tol, nodes_max, mesh_max, nx, nu, nw, nd, ns] = generate_bvpdae(filename);
+    fprintf(fileID_info, '%s\n', filename);
+    test_example = [examples_Dir, filename];
+    [y0, z0, para, t0, tf, t_span, outputFile, alpham, tol, nodes_max, mesh_max, nx, nu, nw, nd, ns] = generate_bvpdae(test_example);
     nodes_max = 20000;
 %     outputFile = [outputFile,'_sequentialQR'];
     mps_solver(y0, z0, para, t0, tf, outputFile, alpham, tol, nodes_max, mesh_max, nx, nu, nw, nd, ns);
